3f78f08e5ea9a6aae6d823c703e2960cc73678ec
tdepasquale <53018681+tdepasquale@users.noreply.github.com>
2020-12-16 18:53:14 -0500

    Added basic authentication

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -5,5 +5,7 @@ namespace API.Entities
         public int Id { get; set; }
         //DotNet Identity expects UserName to be capitalized like this.
         public string UserName { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
     }
 }
