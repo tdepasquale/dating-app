fb4e0823bf76c928f59e72cff6c949c5475166c0
tdepasquale <53018681+tdepasquale@users.noreply.github.com>
2021-01-28 19:25:37 -0500

    Formatted dates using pipes

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -7,6 +7,7 @@ import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { TabsModule } from 'ngx-bootstrap/tabs';
+import { TimeagoModule } from 'ngx-timeago';
 import { ToastrModule } from 'ngx-toastr';
 @NgModule({
   declarations: [],
@@ -22,6 +23,7 @@ import { ToastrModule } from 'ngx-toastr';
     BsDatepickerModule.forRoot(),
     PaginationModule.forRoot(),
     ButtonsModule.forRoot(),
+    TimeagoModule.forRoot(),
   ],
   exports: [
     BsDropdownModule,
@@ -32,6 +34,7 @@ import { ToastrModule } from 'ngx-toastr';
     FileUploadModule,
     PaginationModule,
     ButtonsModule,
+    TimeagoModule,
   ],
 })
 export class SharedModule {}
