45b044c464a72abb4ca71c3ef1f4f44a3030801a
tdepasquale <53018681+tdepasquale@users.noreply.github.com>
2021-02-04 01:49:41 -0500

    Added likes backend

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -4,26 +4,25 @@ using API.Extensions;
 
 namespace API.Entities
 {
-    public class AppUser
-    {
-        public int Id { get; set; }
-        //DotNet Identity expects UserName to be capitalized like this.
-        public string UserName { get; set; }
-        public byte[] PasswordHash { get; set; }
-        public byte[] PasswordSalt { get; set; }
-        public DateTime DateOfBirth { get; set; }
-        public string KnownAs { get; set; }
-        public DateTime Created { get; set; } = DateTime.Now;
-        public DateTime LastActive { get; set; } = DateTime.Now;
-        public string Gender { get; set; }
-        public string Introduction { get; set; }
-        public string LookingFor { get; set; }
-        public string Interests { get; set; }
-        public string City { get; set; }
-        public string Country { get; set; }
-        public ICollection<Photo> Photos { get; set; }
-        // public int GetAge(){
-        //     return DateOfBirth.CalculateAge();
-        // }
-    }
+  public class AppUser
+  {
+    public int Id { get; set; }
+    //DotNet Identity expects UserName to be capitalized like this.
+    public string UserName { get; set; }
+    public byte[] PasswordHash { get; set; }
+    public byte[] PasswordSalt { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string KnownAs { get; set; }
+    public DateTime Created { get; set; } = DateTime.Now;
+    public DateTime LastActive { get; set; } = DateTime.Now;
+    public string Gender { get; set; }
+    public string Introduction { get; set; }
+    public string LookingFor { get; set; }
+    public string Interests { get; set; }
+    public string City { get; set; }
+    public string Country { get; set; }
+    public ICollection<Photo> Photos { get; set; }
+    public ICollection<UserLike> LikedByUsers { get; set; }
+    public ICollection<UserLike> LikedUsers { get; set; }
+  }
 }
